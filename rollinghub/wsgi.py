3ebbd412eb75fba2cedffaed400af25f49780336
Alex Dawn <al_4242@hotmail.co.uk>
2019-08-07 21:28:29 +0100

    Somehow fixed waitress
    by wrapping the app in a logger?

--- rollinghub/wsgi.py
+++ rollinghub/wsgi.py
@@ -1,16 +1,7 @@
 from rollinghub import create_app
 import waitress
 import os
-import sys
-import logging
-waitress_logger = logging.getLogger('waitress')
-waitress_logger.setLevel(logging.INFO)
-file_handler = logging.FileHandler(filename='tmp.log')
-handler = logging.StreamHandler(sys.stdout)
-handler.setLevel(logging.DEBUG)
-waitress_logger.addHandler(handler)
-waitress_logger.addHandler(file_handler)
+from paste.translogger import TransLogger
 
 app = create_app()
-waitress_logger.warning("FOO")
-waitress.serve(app, port=os.environ['PORT'])
+waitress.serve(TransLogger(app), port=os.environ['PORT'])
