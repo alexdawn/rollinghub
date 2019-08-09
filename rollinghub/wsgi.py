3331c7b869717029e318e73bb98859ca935f6526
Alex Dawn <al_4242@hotmail.co.uk>
2019-08-09 19:08:39 +0100

    Fix entry point to app instance

--- rollinghub/wsgi.py
+++ rollinghub/wsgi.py
@@ -1,10 +1,3 @@
 from rollinghub import create_app
-import waitress
-import os
-from paste.translogger import TransLogger
-import logging
 
-
-logging.warning("Running on port {}".format(os.environ['PORT']))
 app = create_app()
-waitress.serve(TransLogger(app), port=os.environ['PORT'])
