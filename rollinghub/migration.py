dada818dfd2b50377707283544a06d630258d43b
Alex Dawn <al_4242@hotmail.co.uk>
2019-08-09 23:29:32 +0100

    move init outside of flask app

---
+++ rollinghub/migration.py
@@ -0,0 +1,20 @@
+import psycopg2
+import psycopg2.extras
+import os
+
+
+def get_db():
+    url = os.environ['DATABASE']
+    sslmode = 'require'
+    db = psycopg2.connect(url, sslmode=sslmode)
+    return db, db.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+
+def init_db():
+    db, cur = get_db()
+    with open('./schema.sql', 'r') as f:
+        cur.execute(f.read())
+    db.commit()
+
+    
+init_db()
